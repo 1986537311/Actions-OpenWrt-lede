683062bb15a2fa98009fd07230eaa90004129d5d
未来 <aiastia@users.noreply.github.com>
2024-06-03 17:15:37 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
+#echo 'src-git diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
